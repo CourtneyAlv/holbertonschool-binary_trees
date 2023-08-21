0b1991857e4c0d74c4f19f80b69ec127611e7e5e
CourtneyAlv <voiceoftruth42@gmail.com>
2023-08-21 15:33:20 -0700

    fixed betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -23,11 +23,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_height, right_height;
+
 	if (tree == NULL)
 	return (0);
 
-	int left_height = binary_tree_height(tree->left);
-	int right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return (left_height - right_height);
 }
