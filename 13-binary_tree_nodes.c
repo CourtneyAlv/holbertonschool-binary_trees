e4bba41b89e4b06da6c49fcb803faa0772da568f
CourtneyAlv <voiceoftruth42@gmail.com>
2023-08-21 11:40:21 -0700

    a dunction that counts nodes with at least one child[D

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,9 +1,16 @@
-#include "binary_trees.c"
+#include "binary_trees.h"
 /**
- *
- *
- *
- *
+ *binary_tree_nodes - counts the nodes with at least one child
+ * @tree: a pointer to the root node of the tree to count the # of nodes
+ * Return: the number of nodes with at least 1 child, tree = NULL, return 0
 */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+	return (0);
+
+	if (tree->left != NULL || tree->right != NULL)
+	return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	else
+	return (0);
+}
