75767d25bc49111e52bc7af85d3ca739a4653fed
CourtneyAlv <voiceoftruth42@gmail.com>
2023-08-20 20:02:14 -0700

    Write a function that checks if a node is a leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,9 +1,14 @@
-#include "binary_trees.c"
+#include "binary_trees.h"
 /**
- *
- *
- *
- *
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to the node to check
+ * Return: 1 if node is leaf, otherwise 0
 */
-void binary_tree_delete(binary_tree_t *tree)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
+	if (node == NULL)
+	return (0);
+
+	/* node is a leaf if both left and right childs are NULL */
+	return (node->left == NULL && node->right == NULL);
+}
