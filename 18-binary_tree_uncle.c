42f8af63acb32ebc79fa6aeb98053c2e558e9750
CourtneyAlv <voiceoftruth42@gmail.com>
2023-08-21 15:13:35 -0700

    fixed compile issues

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,7 +9,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	binary_tree_t *grandparent = node->parent->parent;
+	binary_tree_t *grandparent;
+	grandparent = node->parent->parent;
 
 	if (grandparent->left == node->parent)
 	{
